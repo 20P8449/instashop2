3b57cd83caf5273d448d5b5603d640ba4283dde4
Mohamed Barakat <119812222+20P8449@users.noreply.github.com>
2023-11-30 22:30:33 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -16,3 +16,28 @@ window.onscroll = () =>{
         header.classlist.remove('active');
     }
 }
+
+let countDate = new Date( 'june 1, 2021 00:00:00').getTime();
+function CountDown(){
+    let now = new Date().getTime();
+    gap = countDate - now;
+
+    let second = 1000;
+    let minute = second * 60;
+    let hour = minute * 60;
+    let day = hour * 24;
+
+    let d = Math. floor (gap / (day));
+    let h = Math.floor((gap % (day)) / (hour));
+    let m = Math. floor ((gap % (hour)) / (minute));
+    let s = Math. floor((gap % (minute)) / (second));
+
+    document.getElemetById('day').innertext = d;
+    document. getElemetById('hour').innertext = h;
+    document.getElemetById('minute').innertext = m;
+    document. getElemetById('second').innertext = s;
+}
+
+setInterval(function(){
+    CountDown();
+},1000)
